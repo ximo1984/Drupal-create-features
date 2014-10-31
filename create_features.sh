2d57ecd0ac7fa58a844d66aed30397a260a0fa70
David Hernández <davidhernandezruiz@gmail.com>
2014-10-31 11:08:39 +0100

    Added support for commerce exports. Removed need of the argument.

--- create_features.sh
+++ create_features.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-categories=( taxonomy menu_custom filter field_base field_instance node views_view user_role user_permission rules_config profile2_type flag elysia_cron feeds_importer)
-
+CATEGORIES=( taxonomy menu_custom filter field_base field_instance node commerce_customer commerce_product_type profile2_type flag views_view user_role user_permission rules_config elysia_cron feeds_importer)
 echo $1 | grep -i 'help' > /dev/null
 
 if [ -z "$1" -o $? == 0 ]
 then
   echo "This scripts exports the configuration to features using a layered approach."
   echo "It will export features in a specific order and will enable them."
-  echo "Usage: create_features.sh [profile name]"
+  echo "Usage:"
+  echo "From the profile root page: ./scripts/create_features.sh"
   exit 1
 fi
 
-PAQUETE=$1
+PACKAGE=`ls | sed -rn 's/([a-zA-Z0-9\_]+)\.info/\1/p'`
 
 read -p "You are going to create / recreate all the features. Are you sure you want to continue? [Y/n] " -n 1 -r
 echo
@@ -21,15 +21,15 @@ if [[ $REPLY =~ ^[Yy]$ ]]
 then
   for i in ${categories[@]}
   do
-    feature_name=${PAQUETE}_$i
+    feature_name=${PACKAGE}_$i
     if [ -f "modules/feature/${feature_name}/${feature_name}.info" ]
     then
       echo "Feature ${feature_name} already exists, we will increment the version number"
-      drush fe $feature_name "$i:" --destination=profiles/${PAQUETE}/modules/feature --version-increment -y
+      drush fe $feature_name "$i:" --destination=profiles/${PACKAGE}/modules/feature --version-increment -y
     else
       echo "We will create the feature ${feature_name}"
-      drush fe $feature_name "$i:" --destination=profiles/${PAQUETE}/modules/feature --version-set="7.x-1.0" -y
-      sed -i "s/package = Features/package = Features ${PAQUETE}/" modules/feature/${feature_name}/${feature_name}.info
+      drush fe $feature_name "$i:" --destination=profiles/${PACKAGE}/modules/feature --version-set="7.x-1.0" -y
+      sed -i "s/package = Features/package = Features ${PACKAGE}/" modules/feature/${feature_name}/${feature_name}.info
       drush en $feature_name -y
     fi
   done
